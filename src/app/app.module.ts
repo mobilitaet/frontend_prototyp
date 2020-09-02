1f677e45c12c86e54203c1c5ca1ff8996f692c9e
Eduard <eduard.stetco@htlpinkafeld.at>
2020-09-02 22:32:45 +0200

    In Porgress: Fixing API Error 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserlistComponent } from './userlist/userlist.component';
@@ -9,21 +8,30 @@ import { environment } from '../environments/environment';
 import { DxDataGridModule } from 'devextreme-angular';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FileuploadComponent } from './fileupload/fileupload.component';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { MessagingService } from './service/messaging.service';
+import { AsyncPipe } from '../../node_modules/@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserlistComponent,
-    FileuploadComponent
-  ],
+  declarations: [AppComponent, UserlistComponent, FileuploadComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    AngularFireModule.initializeApp(environment.firebase),
     DxDataGridModule,
     ReactiveFormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    })
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MessagingService,AsyncPipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyALvURJc8UfaIzU_hgTy5Ndmn_yaK3ZEC8",
+    authDomain: "mobilitaetproto.firebaseapp.com",
+    databaseURL: "https://mobilitaetproto.firebaseio.com",
+    projectId: "mobilitaetproto",
+    storageBucket: "mobilitaetproto.appspot.com",
+    messagingSenderId: "702740206955",
+    appId: "1:702740206955:web:3526f75acba06df7bbe82e"
+  }
 };
 
 /*
